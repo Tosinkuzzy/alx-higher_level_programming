741b776086a897a21de52a10cbdfbad60c522f76
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-10 23:28:30 +0100

    test file

--- test.py
+++ test.py
@@ -2,5 +2,7 @@
 #a = { 'id': 89, 'name': "John", 'projects': [1, 2, 3, 4] }
 #b = a.get('projects')
 #print(b)
-a = { 'id': 89, 'name': "John" }
-print(a['id'])
+#a = { 'id': 89, 'name': "John" }
+#print(a['id'])
+for i in [1, 3, 4, 2]:
+    print(i, end=" ")
