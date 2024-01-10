2cc0bf92ca63471f0fe1fdec33c1c05a7b5ad1e1
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-10 23:22:31 +0100

    testing

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-a = { 'id': 89, 'name': "John", 'projects': [1, 2, 3, 4] }
-b = a.get('projects')
-print(b)
+#a = { 'id': 89, 'name': "John", 'projects': [1, 2, 3, 4] }
+#b = a.get('projects')
+#print(b)
+for i in range(0, 3):
+    print(i, end=" ")
