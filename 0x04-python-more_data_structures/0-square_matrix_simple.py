91f081471d8d5d6396dba0aef325e452b9b02295
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-11 02:45:27 +0100

    executable file 0-main

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/pyton3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), top)) for top in matrix]
+    return [list(map((lambda x: x * x), lm)) for lm in matrix]
