d5e12052e3f398bcf7e0cfe8123944ca6911c6bb
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-11 02:54:50 +0100

    function that replaces all occurrences of an element by another in a new list.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def seacrh_replace(my_list, search, replace):
+def search_replace(my_list, search, replace):
     def s_r_element(elm):
         return (elm if elm != search else replace)
     return list(map(s_r_element, my_list))
