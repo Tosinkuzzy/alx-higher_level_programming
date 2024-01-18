98aa0960357a0029876683f57ce835ba4a51e812
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-19 00:32:44 +0100

    _putchar file

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#include <unistd.h>
+/**
+* _putchar - write the character to stdout
+* @c: The character to print
+* Return: On sucess 1, On error -1 is returned.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
