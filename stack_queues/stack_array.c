cde3044eb052fc5bc38eeae42de74e533c3f18b5
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-20 02:28:29 +0100

    stack_queue

--- stack_queues/stack_array.c
+++ stack_queues/stack_array.c
@@ -16,3 +16,6 @@ int choice;
 while(1)
 {
 printf("\nPerform operation on the stacks:");
+printf("\n1.Push the element\n2.Pop the element\n3.Show\n4.End");
+printf("\n\nEnter the choice: ");
+printf("%d", &choice);
