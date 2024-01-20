7369889c4a3bd1fa60d09a83034aae881dc6c75c
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-20 02:32:08 +0100

    stack_queue

--- stack_queues/stack_array.c
+++ stack_queues/stack_array.c
@@ -19,3 +19,23 @@ printf("\nPerform operation on the stacks:");
 printf("\n1.Push the element\n2.Pop the element\n3.Show\n4.End");
 printf("\n\nEnter the choice: ");
 printf("%d", &choice);
+
+switch(choice)
+{
+case 1:
+push();
+break;
+case 2:
+pop();
+break;
+case 3:
+show();
+case 4:
+exit(0);
+break;
+
+default:
+printf("\nInvalid choice!!");
+}
+}
+}
