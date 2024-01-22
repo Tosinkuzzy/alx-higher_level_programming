3c5eff63fc4dbcc8cc0e4786507fe060e941d487
Tosinkuzzy <Adeleye608@gmail.com>
2024-01-22 15:20:45 +0100

    exceeptions

--- 0x05-python-exceptions/testing.py
+++ 0x05-python-exceptions/testing.py
@@ -6,3 +6,20 @@ while True:
         break
     except ValueError:
         print('Oops! invalid number')
+
+class B(Exception):
+    pass
+class C(B):
+    pass
+class D(C):
+    pass
+for cls in [B, C, D]:
+    try:
+            raise cls()
+    except B:
+        print('B')
+    except C:
+        print('C')
+    except D:
+        print('D')
+
