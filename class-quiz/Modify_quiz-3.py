5c92d3f4e64693dc0f0a437dc1b6c19e2e9787ff
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2024-02-01 12:23:34 +0100

    Create Modify_quiz-3.py

---
+++ class-quiz/Modify_quiz-3.py
@@ -0,0 +1,16 @@
+class Person:
+  def __init__(self, comp, name=None):
+    self.name = name
+    self.comp = comp
+  def name(self):
+    nm = str(input("Enter your name: ")
+    if name == "Tosinkuzzy":
+             print("Already exist")
+    elif name == "Kuzzy":
+             print("Use something different")
+else:
+      print("Quit the program")
+  def comp(self, name):
+    for user in nm:
+      if user not in nm:
+        print("Registration successful")
