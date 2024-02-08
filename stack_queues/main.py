09d5bf660fad7031f9b8ffd4ca7bce8eb3b9e430
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-08 21:44:21 +0100

    list main

---
+++ stack_queues/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+multiplying all int, float and string by 2
+"""
+
+from ch07 import double_stuff
+
+things = [3.5, '9jajollof', 3, 'subscribe']
+double_stuff(things)
+print(things)
