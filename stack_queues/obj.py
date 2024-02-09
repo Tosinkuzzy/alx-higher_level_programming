e5a1ca032ee9e1aaeb2802efe4333dd5bfe9d728
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-09 16:32:55 +0100

    everything is object

---
+++ stack_queues/obj.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Objects represent information and processes,
+bundled together to represent the properties,
+interactions, and behaviour of complex things.
+
+>>> chinese = ['coin', 'string', 'myriad']
+>>> suits = chinese
+>>> suits.pop()
+>>> suits.remove('string')
+>>> suits
+
+"""
+import doctest
+doctest.testmod()
