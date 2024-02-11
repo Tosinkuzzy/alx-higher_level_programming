4b3607dc1acf0db254b4c20f52258fd8503cb1e1
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-11 02:06:55 +0100

    date format

--- stack_queues/date_format.py
+++ stack_queues/date_format.py
@@ -14,7 +14,7 @@
 
 """
 from datetime import date
-sun = date(2014, 5, 13)
-print(date(2014, 5, 19) - sun)
-sun.year
-sun.strftime('%A, %B %d')
+sun = date(2024, 2, 11)
+print(date(2024, 2, 11) - sun)
+print(sun.year)
+print(sun.strftime('%A, %B %d'))
