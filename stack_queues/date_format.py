3f2ef005236869e5e4abc72d4e2840f1aa911ab9
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-11 01:49:08 +0100

    the object metaphor

---
+++ stack_queues/date_format.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    The Object Metaphor
+    Specialized Object Syntax and associated
+    Terminology
+"""
+>>> from datetime import date
+>>> sun = date(2024, 2, 11)
+>>> print(date(2024, 2, 11)
