85a235e22fa1fbdcd8135879743a2bbe6a9e2c3f
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-11 02:39:42 +0100

    boolean

--- stack_queues/hi.py
+++ stack_queues/hi.py
@@ -3,7 +3,7 @@
 i = '1234'.isnumeric()
 s = '1234'.isalpha()
 n = 'Is coOL pYThon dO Ouy enojy ti'.swapcase()
-k = 'subscribe'.upper().endswith('CRIB')
+k = 'subscribe'.upper().endswith('CRIBE')
 print(k)
 print(n)
 print(i)
