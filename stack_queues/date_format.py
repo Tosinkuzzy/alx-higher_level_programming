fb00af1cdaa82fe3fc5525994d7d45a5a3525837
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-11 02:02:28 +0100

    date format

--- stack_queues/date_format.py
+++ stack_queues/date_format.py
@@ -5,6 +5,27 @@
     Specialized Object Syntax and associated
     Terminology
 """
+
+"""
 >>> from datetime import date
 >>> sun = date(2024, 2, 11)
 >>> print(date(2024, 2, 11)
+>>> print(sun)
+
+"""
+"""
+from datetime import date
+
+sun = date(2024, 2, 11)
+print(date(2024, 2, 11)
+
+print(sun.year)
+print(sun.month)
+print(sun.day)
+print(sun.strftime('%A, %B %d'))
+"""
+from datetime import date
+tues = date(2014, 5, 13)
+print(date(2014, 5, 19) - tues)
+tues.year
+tues.strftime('%A, %B %d')
