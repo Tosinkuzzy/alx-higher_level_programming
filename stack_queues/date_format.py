29411d557e2cbc658634596836e575e269e634ee
Tosinkuzzy <Adeleye608@gmail.com>
2024-02-11 02:04:43 +0100

    date format

--- stack_queues/date_format.py
+++ stack_queues/date_format.py
@@ -9,23 +9,12 @@
 """
 >>> from datetime import date
 >>> sun = date(2024, 2, 11)
->>> print(date(2024, 2, 11)
+>>> print(date(2024, 2, 11) - sun)
 >>> print(sun)
 
 """
-"""
 from datetime import date
-
-sun = date(2024, 2, 11)
-print(date(2024, 2, 11)
-
-print(sun.year)
-print(sun.month)
-print(sun.day)
-print(sun.strftime('%A, %B %d'))
-"""
-from datetime import date
-tues = date(2014, 5, 13)
-print(date(2014, 5, 19) - tues)
-tues.year
-tues.strftime('%A, %B %d')
+sun = date(2014, 5, 13)
+print(date(2014, 5, 19) - sun)
+sun.year
+sun.strftime('%A, %B %d')
